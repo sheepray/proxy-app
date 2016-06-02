625189467d062cdd36b9abc3fdd24252d251e61d
sheepray <sheepray1992@gmail.com>
2016-06-02 21:20:02 +0300

    proxy auto login done

--- index.js
+++ index.js
@@ -18,27 +18,26 @@ function initialize(){
 
 		mainWindow = new BrowserWindow(windowOptions)
 		//mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
-
+		
 		mainWindow.on('closed', function(){
 				mainWindow = null
 		})
 
 		const session = electron.session
-		const ses = session.fromPartition('firstSession')
-		const remoteProxy = 'https://httpbin.org/ip'
-		const targetURL = 'http://www.github.com'
+		const ses = session.fromPartition('proxySession')
+		const remoteProxy = 'http://127.0.0.1:3128' //replace it with your proxy address along with port
+		const targetURL = 'http://www.github.com' //the url you want to connect to.
 
-		ses.setProxy({proxyRules: remoteProxy}, function(){
-				console.log('proxy used')
-				mainWindow.loadURL(targetURL)
-		})
-
+		mainWindow.webContents.session.setProxy({proxyRules: remoteProxy}, function () {
+        mainWindow.loadURL(targetURL);
+    });
 	}
 
-	/* replace the username and secret with your own credentials */
+	/* replace the username and password with your own credentials */
 	app.on('login', function(event, webContents, request, authInfo, callback) {
-		event.preventDefault();
-		callback('username', 'secret');
+		console.log('login event triggered');
+		event.preventDefault()
+		callback('username', 'password')
 	})
 
 	/* create window when app loaded by node */
