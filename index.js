35f6835987fa797ffb787b37547a6670272fad85
sheepray <sheepray1992@gmail.com>
2016-06-03 13:13:07 +0300

    added comments

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const BrowserWindow = electron.BrowserWindow
 
 var mainWindow = null
 
+/* initialize application */
 initialize()
 
 function initialize(){
+
+	/* create a window for the app */
 	function createWindow(){
 		var windowOptions = {
 			width: 800,
@@ -16,26 +19,27 @@ function initialize(){
 			minWidth:680
 		}
 
+		/* create the window with settings */
 		mainWindow = new BrowserWindow(windowOptions)
+
+		/* populate the content of the window using a file */
 		//mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
 
+		/* when window is closed */
 		mainWindow.on('closed', function(){
 				mainWindow = null
 		})
 
-		const session = electron.session
-		const ses = session.fromPartition('proxySession')
 		const remoteProxy = 'http://127.0.0.1:3128' //replace it with your proxy address along with port
 		const targetURL = 'http://www.stackexchange.com' //the url you want to connect to.
 		const referrerURL = 'http://www.google.com' //referrer url
 
+		/* set proxy */
 		mainWindow.webContents.session.setProxy({proxyRules: remoteProxy}, function (){
-			//mainWindow.loadURL(targetURL)
-			const httpExtraHeader = {httpReferrer: referrerURL + '\n'}
-			//mainWindow.loadURL(targetURL, httpExtraHeader)
-			mainWindow.webContents.loadURL(targetURL, httpExtraHeader)
+			const httpReferrerHeader = {httpReferrer: referrerURL + '\n'}
+			mainWindow.loadURL(targetURL, httpReferrerHeader)
 		})
-	}
+	}// end of create window function.
 
 	/* replace the username and password with your own credentials */
 	app.on('login', function(event, webContents, request, authInfo, callback) {
@@ -54,4 +58,4 @@ function initialize(){
 			app.quit()
 		}
 	})
-}
+}// end of initialize function.
